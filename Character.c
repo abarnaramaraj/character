01fa76ea294b2eb1af704b04159e0eb5320281a6
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-02-19 20:47:33 +0530

    Create Character.c

---
+++ Character.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf(“enter the char”);
+scanf("%c,&c);
+if((c>='a'&&c<='z')||(c>='A'&&c<'Z'))
+print(“%c is an alphabet”)
+else
+printf(“%c is not an alphaber”)
+return 0;
+}
